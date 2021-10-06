9a3c074401a07c146bb6eac818992a8b649be87d
CarOL <32268783+hx-w@users.noreply.github.com>
2021-10-06 16:55:55 +0800

    Update log.vue

--- src/views/log.vue
+++ src/views/log.vue
@@ -3,7 +3,7 @@
     <el-header><h1 align="center">BILIBILI-HELPER日志</h1></el-header>
     <el-main>
       <el-card class="box-card">
-      <div class="text-wrapper" align="center" style="font-size:18px;"><span>{{ log }}</span></div>
+      <div class="text-wrapper" style="font-size:18px;"><span>{{ log }}</span></div>
       </el-card>
     </el-main>
   </el-container>
