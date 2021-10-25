45cc47ef9bd20d0fd153f620c829f0adc24c675d
hx-w <herixth@163.com>
2021-10-25 20:37:21 +0800

    reset

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,12 @@ import VueRouter from 'vue-router' //引入 Vue 路由
 Vue.use(VueRouter) //安装插件
 
 const router = new VueRouter({
-  mode: 'history',
   base: '/',
   routes: [
-    // { path: '/', redirect: '/auto-checkin' },
-    { path: '/', name: 'scu', component: () => import('@/views/scu-check') },
+    { path: '/', redirect: '/auto-checkin' },
     { path: '/log', name: 'log', component: () => import('@/views/log') },
     { path: '/config', name: 'config', component: () => import('@/views/config') },
+    { path: '/auto-checkin', name: 'scu', component: () => import('@/views/scu-check') },
     { path: '*', component: () => import('@/views/scu-check') },
   ]
 })
