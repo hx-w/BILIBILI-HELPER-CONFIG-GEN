94d5360b2c3d31a2820e4e35e27e5056941ce543
hx-w <herixth@163.com>
2021-10-25 13:43:33 +0800

    +scu-check

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ const router = new VueRouter({
     { path: '/', redirect: '/config' },
     { path: '/log', name: 'log', component: () => import('@/views/log') },
     { path: '/config', name: 'config', component: () => import('@/views/config') },
+    { path: '/scu', name: 'scu', component: () => import('@/views/scu-check') },
   ]
 })
 
