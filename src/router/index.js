0cadb0416c97905eb030c285921f7cf54ba649ba
hx-w <herixth@163.com>
2021-10-25 21:05:04 +0800

    split scubot to sperate repo

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,9 @@ Vue.use(VueRouter) //安装插件
 const router = new VueRouter({
   base: '/',
   routes: [
-    { path: '/', redirect: '/auto-checkin' },
+    { path: '/', redirect: '/config' },
     { path: '/log', name: 'log', component: () => import('@/views/log') },
     { path: '/config', name: 'config', component: () => import('@/views/config') },
-    { path: '/auto-checkin', name: 'scu', component: () => import('@/views/scu-check') },
-    { path: '*', component: () => import('@/views/scu-check') },
   ]
 })
 
